61639883f3eecc9ef1c51f602a7e349b5e6ba0b4
TimCalavera <timucinberk2@gmail.com>
2019-12-21 14:34:04 +0300

    CatalogFactory start

--- StockaidNetbeans/src/domain/CatalogFactory.java
+++ StockaidNetbeans/src/domain/CatalogFactory.java
@@ -3,4 +3,17 @@ package domain;
 
 public class CatalogFactory {
     
+        
+        // Instantiation
+        ProductCatalog productCatalog;
+        SupplierCatalog supplierCatalog;
+        OrderCatalog orderCatalog;
+        
+        private CatalogFactory(){
+        productCatalog = null;
+        supplierCatalog = null;
+        orderCatalog = null;
+        }
+        
+
 }
